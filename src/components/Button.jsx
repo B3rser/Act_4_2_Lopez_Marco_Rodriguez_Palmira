27dd2a8248288bace9c5a188a5859b225124ed6f
Pgrg <palmirag211@gmail.com>
2025-05-18 13:25:34 -0600

    implementacion del componente button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './css/Button.css';
 
-export function Button() {
-    return (
-        <div>Buttons</div>
-    )
-}
+const Button = ({ label, priority = 'primary', state = 'default', onClick }) => {
+  const isDisabled = state === 'disabled';
+
+  // Solo agregar clase hover si el estado no es explicitamente hover o disabled
+  const className = `custom-button ${priority} ${state !== 'default' ? state : ''}`;
+
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import Button from '../components/Button';
 
 export function Components() {
     return (
-        <div>components</div>
+        <div>
+            <Button
+                label="Primary"
+                priority="primary"
+                onClick={() => alert('¡Clic real detectado!')}
+            />
+            <Button
+                label="Primary D"
+                priority="primary"
+                state="disabled"
+            />
+
+
+            <Button
+                label="Alternative"
+                priority="alternative"
+            
+            />
+            <Button
+                label="Alternative D"
+                priority="alternative"
+                state="disabled"
+            
+            />
+
+
+            <Button
+                label="Secondary"
+                priority="secondary"
+            />
+            <Button
+                label="Secondary D"
+                priority="secondary"
+                state="disabled"
+            />
+
+            
+
+        </div>
     )
 }
